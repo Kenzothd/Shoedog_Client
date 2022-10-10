737a23b39a12e1250ea57a2ddee5ce8efa09ecd3
Kenzothd <Kenzothd@hotmail.com>
2022-10-10 15:44:39 +0800

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,14 @@
+import { Outlet, useNavigate, Link } from "react-router-dom";
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+
+function Navbar() {
+  return (
+    <>
+      <h1>this is the nav bar</h1>
+      <Outlet />
+    </>
+  );
+}
+
+export default Navbar;
